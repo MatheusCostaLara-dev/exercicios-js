c41e88e95c0137df3bd96f7faaae6f6c8b910e80
MatheusCostaLara-dev <matheuscostalara@hotmail.com>
2022-11-24 19:10:40 -0300

    first commit

---
+++ fundamentos/comentario.js
@@ -0,0 +1,8 @@
+// comentário de uma linha
+console.log("Linha 1");
+
+/*
+    Comentário de 
+    Multiplas linhas
+*/
+console.log("Linha 2");
---
+++ fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+//sentença de código
+console.log("sentença de código");
+
+// bloco de código
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!')//padrão do curso 
+    }
+}
